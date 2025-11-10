625e156d91a5358be7b3e320410c5b41a6ea0042
Hasan Borshalli <hasanborshalli@gmail.com>
2025-11-10 12:24:03 +0200

    remove import

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Hero } from './components/Hero';
-import { History } from './components/History';
-import { FunFacts } from './components/FunFacts';
-import { Gallery } from './components/Gallery';
-import { BananaTypes } from './components/BananaTypes';
-import { Footer } from './components/Footer';
+import { Hero } from "./components/Hero";
+import { History } from "./components/History";
+import { FunFacts } from "./components/FunFacts";
+import { Gallery } from "./components/Gallery";
+import { BananaTypes } from "./components/BananaTypes";
+import { Footer } from "./components/Footer";
 export function App() {
-  return <div className="bg-yellow-50 w-full min-h-screen">
-      <Hero />
-      <History />
-      <FunFacts />
-      <Gallery />
-      <BananaTypes />
-      <Footer />
-    </div>;
+    return (
+        <div className="bg-yellow-50 w-full min-h-screen">
+            <Hero />
+            <History />
+            <FunFacts />
+            <Gallery />
+            <BananaTypes />
+            <Footer />
+        </div>
+    );
 }
